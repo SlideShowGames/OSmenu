e113c376f71bbe7e690f244c25d2ab6b837553d1
Bradley Stratton <108354503+SlideShowGames@users.noreply.github.com>
2022-09-28 16:55:41 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+UserName = input("make a name")
+def OSmenu():
+    print('Welcome To iCal OS v2.1.0 {}'.format(UserName))
+    MainMenu = input("""
+                   What Would You Like To Do?
+                   [1]. Exit os
+                   """)
+
+OSmenu()
